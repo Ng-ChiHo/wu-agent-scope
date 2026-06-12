b2a695f5c0308734fbb4081dd0d3eb73d9ce8d2a
chiho <ngchiho2015@sina.com>
2026-06-12 22:51:08 +0800

    initial project setting

---
+++ src/main/java/com/chiho/wuagentscope/WuAgentScopeApplication.java
@@ -0,0 +1,13 @@
+package com.chiho.wuagentscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuAgentScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuAgentScopeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chiho/wuagentscope/WuAgentScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiho.wuagentscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuAgentScopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
